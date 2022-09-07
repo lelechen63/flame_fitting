425d94015a142db4fa87e437fddc610ff9bd611e
Lele Chen <lelechen@US01797MC.local>
2022-09-07 14:03:39 -0700

    fix bugs for libing

--- models/FLAME.py
+++ models/FLAME.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
 """
 Author: Soubhik Sanyal
 Copyright (c) 2019, Soubhik Sanyal
@@ -12,27 +16,6 @@ import torch.nn.functional as F
 import sys
 sys.path.append('/home/us000218/lelechen/github/CIPS-3D/photometric_optimization/models')
 
-# from lbs import lbs, batch_rodrigues, vertices2landmarks
-
-# -*- coding: utf-8 -*-
-
-# Max-Planck-Gesellschaft zur Förderung der Wissenschaften e.V. (MPG) is
-# holder of all proprietary rights on this computer program.
-# You can only use this computer program if you have closed
-# a license agreement with MPG or you get the right to use the computer
-# program from someone who is authorized to grant you that right.
-# Any use of the computer program without a valid license is prohibited and
-# liable to prosecution.
-#
-# Copyright©2019 Max-Planck-Gesellschaft zur Förderung
-# der Wissenschaften e.V. (MPG). acting on behalf of its Max Planck Institute
-# for Intelligent Systems. All rights reserved.
-#
-# Contact: ps-license@tuebingen.mpg.de
-
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import division
 
 import numpy as np
 
